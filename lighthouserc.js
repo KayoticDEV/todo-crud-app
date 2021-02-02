c581f3fe750f99366e9799c5fd6a62f542916132
KayoticDEV <dev.harshit.1024@gmail.com>
2021-02-02 07:32:58 -0800

    Updating assertion params

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,16 +2,16 @@ module.exports = {
     ci: {
       collect: {
         staticDistDir: './dist/todo-crud-app/',
-        method: "node",
+        method: 'node',
         additive: false,                            
         headful: false,
         assert: {
           preset: 'lighthouse:no-pwa',
           assertions: {
-            'categories:accessibility': ['error', {aggregationMethod: 'optimistic', minScore: 1}],
-            'categories:performance': ['warn', {aggregationMethod: 'optimistic', minScore: 1}],
-            'categories:best-practices': ['error', {aggregationMethod: 'optimistic', minScore: 1}],
-            'categories:seo': ['error', {aggregationMethod: 'optimistic', minScore: 1}]            
+            'categories:accessibility': ['error', {aggregationMethod: 'optimistic', minScore: 0.40}],
+            'categories:performance': ['warn', {aggregationMethod: 'optimistic', minScore: 0.40}],
+            'categories:best-practices': ['error', {aggregationMethod: 'optimistic', minScore: 0.65}],
+            'categories:seo': ['error', {aggregationMethod: 'optimistic', minScore: 0.60}]            
           }
         },
       },
